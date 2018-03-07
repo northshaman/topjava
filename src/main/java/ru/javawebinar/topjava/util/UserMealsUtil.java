d69d2992395f1fac10dc757563a9a54c04ab70d2
northshaman <north-shaman@mail.ru>
2018-03-07 13:49:24 +0500

    MyHW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -29,6 +29,7 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        System.out.println("My Home Work!");
         // TODO return filtered list with correctly exceeded field
         return null;
     }
